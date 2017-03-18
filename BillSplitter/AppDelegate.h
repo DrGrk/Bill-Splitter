60f64c871ca79928c53b7717fda3ad435317128a
David Guichon <davidrguichon@gmail.com>
2017-03-18 13:16:24 -0400

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by David Guichon on 2017-03-18.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
